50912c2b4ded3b461fb352a829ad7a911b21a310
TimothyAkana <TimothyAkana@gmail.com>
2021-04-03 20:30:01 -0700

    On submitting search form, data is loaded into state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Search from './Search.jsx';
 
 export default function App(props) {
+  const [parkingLots, setParkingLots] = useState([]);
+
+  const searchLocation = (searchTerm) => {
+    axios.get(`/parking/location/${searchTerm}`)
+      .then(results => setParkingLots(results.data.businesses))
+      .catch(err => console.log(err));
+  }
+
   return (
     <div>
       <div>Air Garage Parking Lots</div>
-      <Search />
+      <Search searchLocation={searchLocation} />
     </div>
   )
 }
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 
-export default function Search(props) {
+export default function Search({ searchLocation }) {
   const [search, setSearch] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(search);
+    searchLocation(search);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Search Location:
-        <input type="text" value={search} onChange={e => setSearch(e.target.value)}/>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
       </label>
     </form>
   )
